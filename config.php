81ef2f1ef25a1e62a93d6663131795b46750c005
Vishnuat18 <vishnurajan24766@gmail.com>
2026-07-18 02:19:01 +0530

    Security: Untrack config.php and ignore it

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// PHP local configuration file - DO NOT commit to Git.
-// Copy this to config.php and update with your actual credentials.
-
-define('RESEND_API_KEY', 're_EW4LtVCq_3NBVyot9jEYuME37Fz4PNmjo');
-define('RESEND_FROM', 'onboarding@resend.dev');
-define('RESEND_TO', 'vishnurajan24766@gmail.com');
-?>
